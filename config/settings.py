60c3888efaeae29306f50174b35412013bfd706d
Seiya Asahina <seiya@asahina>
2024-07-21 13:09:44 +0900

    add

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9w@vg!_m8nn%htbj8)^_kjdk2cxb$jx!!g$35)z%el&r&y1%cj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
